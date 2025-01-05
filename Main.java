ffc067bdc54e93729fe2dd1e632c2a1ec4ed6615
evanotoole123 <evankarlotoole@gmail.com>
2025-01-05 12:50:49 -0800

    program succesfully creates a text file for a group based on commandline input

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+
+import com.sun.security.auth.module.UnixSystem;
+
 public class Main{
     public static void main(String[] args){
-        if (args[0] == "make group");
+
+        if (args[0].equals("make group")){
+            User user = new User();
+            user.setGroup(args[1]);
+            System.out.println("succesfully made group: " + group);
+        }
     }
 }
--- Persistence.java
+++ Persistence.java
@@ -1,17 +1,10 @@
-
-import com.sun.source.doctree.SerialFieldTree;
-import java.lang.classfile.instruction.ThrowInstruction;
-import java.time.chrono.ThaiBuddhistChronology;
-
-
-
 public class Persistence{
     public static void createGroup(String group){
         CRUD.createGroup(group);
     }
 
     public static void createEvent(String group, String event, String time){
-
+        
     }
 
     public static void searchEvents(String event, String group){
